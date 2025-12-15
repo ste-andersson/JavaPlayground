e58c702b896971708f048be62cda61aa97c8dbcb
Stefan Andersson <stefan.andersson@sveki.se>
2025-12-15 23:52:29 +0100

    Remove all old code and start over

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,29 @@
 public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("=== KAPITEL 1: LOGIK ===");
-
-        // Vi anropar metoder i LogicOps-klassen genom att skriva: Klassnamn.Metodnamn()
-        int price = LogicOps.getTicketPrice(70);
-        System.out.println("Biljettpris: " + price);
-
-        String vader = LogicOps.checkWeather(25);
-        System.out.println("Väder: " + vader);
-
-
-        System.out.println("\n=== KAPITEL 2: ARRAYS ===");
-
-        int[] data = {10, 20, 30, 5, 99};
-
-        int total = ArrayOps.sumArray(data);
-        System.out.println("Summa: " + total);
-
-        // Här testar vi din nya metod
-        int max = ArrayOps.findMax(data);
-        System.out.println("Största talet: " + max); // Borde bli 99 (när du löst den)
-
+    public static void main(String[] args){
+        //print a string message
+        String message = "Hallå där!";
+        System.out.println(message);
+        //print the same thing but upper case
+        String messageUpperCase = message.toUpperCase();
+        System.out.println(messageUpperCase);
+        //print the first word
+        if (message.indexOf(" ") != -1) {
+           String firstWord = message.substring(0, message.indexOf(" "));
+           System.out.println(firstWord);
+        } else {
+           String firstWord = message;
+           System.out.println(firstWord);
+        }
+        //Count occurances of a letter
+        char letter = 'a';
+        int count = 0;
+        for (int i = 0; i < message.length(); i++) {
+            char letterChecked = message.charAt(i);
+            if (letterChecked == letter || letterChecked == Character.toUpperCase(letter)) {
+                count++;
+            }
+        }
+        System.out.println("Det finns " + count + " stycken " + letter + " i texten.");
     }
 }
