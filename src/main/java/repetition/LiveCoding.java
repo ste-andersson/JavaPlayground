01db83cefec31599265f7a20b42c7d63e75e4ff1
Stefan Andersson <stefan.andersson@sveki.se>
2025-12-26 13:25:11 +0100

    Add isPalindrome method to LiveCoding and some comments for FizzBuzz in LiveCoding.

--- src/main/java/repetition/LiveCoding.java
+++ src/main/java/repetition/LiveCoding.java
@@ -3,15 +3,27 @@ package repetition;
 public class LiveCoding {
 
     public static void main(String[] args) {
+        //Add values for FizzBuzz-test
         int testRange = 40;
         int testDivider1 = 3;
         int testDivider2 = 4;
         String testWord1 = "Hey";
         String testWord2 = "Ho";
+
+        //Create the array
         String[] testResult = fizzBuzzTest(testRange, testDivider1, testDivider2, testWord1, testWord2);
+
+        //Loop out the array
         for (int i = 1; i <= testRange; i++) {
             System.out.println(testResult[i-1]);
         }
+
+        String testWordTrue = "apa";
+        String testWordFalse = "apan";
+
+        System.out.println(isPalindrome(testWordFalse));
+
+
     }
 
     public static String[] fizzBuzzTest(int range, int divider1, int divider2, String word1, String word2) {
@@ -29,4 +41,20 @@ public class LiveCoding {
         }
         return result;
     }
+
+    public static boolean isPalindrome(String palindromeWord) {
+        String word = palindromeWord;
+        String wordBackwards = "";
+        for (int i = word.length(); i > 0; i--) {
+            wordBackwards = wordBackwards + word.charAt(i-1);
+            }
+        if (word.equals(wordBackwards)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
 }
